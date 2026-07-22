342c655c949c14402fe657207ad252747df5a606
edgcsan77 <edgcsan@gmail.com>
2026-07-22 14:10:29 -0500

    Update verifiable_flow.py

--- app/verifiable_flow.py
+++ app/verifiable_flow.py
@@ -20,9 +20,9 @@ VERIFIABLE_PROVIDER_INSTANCE = (
 VERIFIABLE_TIMEOUT_SEC = int(
     os.getenv(
         "RFC_VERIFIABLE_TIMEOUT_SEC",
-        "3600",
+        "7200",
     )
-    or "3600"
+    or "7200"
 )
 
 
